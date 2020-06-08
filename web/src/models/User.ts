46cfeb8a245cb41c60323a0ec3336ecea98eb0ac
Paisit Jarunnamsiri <paisit.j@gmail.com>
2020-06-08 19:00:59 +0700

    Section 13 - UserCollection

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,5 @@
 import { Model } from "./Model";
+import { Collection } from "./Collection";
 import { Attributes } from "./Attributes";
 import { ApiSync } from "./ApiSync";
 import { Eventing } from "./Eventing";
@@ -17,6 +18,13 @@ export class User extends Model<UserProps> {
       new Attributes<UserProps>(attrs),
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
+    );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(
+      rootUrl,
+      (json: UserProps) => User.buildUser(json)
     );
   }
 }
